5d14a0772ca2736aa809587309c73a72d74e2a57
Flannon Jackson <flannon@nyu.edu>
2019-01-11 16:18:08 -0500

    changed appnode to node

--- bin/hosts.sh
+++ bin/hosts.sh
@@ -3,9 +3,9 @@
 # OpenShift cluster nodes
 [[ $(grep '172.99.36.254 control.lab.example.com control' /etc/hosts) ]] || echo '172.99.36.254 control.lab.example.com control' >> /etc/hosts
 
-[[ $(grep '172.99.36.6 master.lab.example.com master' /etc/hosts) ]] || echo '172.99.36.8 master.lab.example.com master' >> /etc/hosts
+[[ $(grep '172.99.36.6 master.lab.example.com master' /etc/hosts) ]] || echo '172.99.36.6 master.lab.example.com master' >> /etc/hosts
 
-[[ $( grep '172.99.36.7 appnode0.lab.example.com appnode0' /etc/hosts) ]] || echo '172.99.36.9 appnode0.lab.example.com appnode0' >> /etc/hosts
+[[ $( grep '172.99.36.7 node1.lab.example.com node1' /etc/hosts) ]] || echo '172.99.36.7 node1.lab.example.com node1' >> /etc/hosts
 
-[[ $( grep '172.99.36.8 appnode1.lab.example.com appnode1' /etc/hosts) ]] || echo '172.99.36.9 appnode0.lab.example.com appnode1' >> /etc/hosts
+[[ $( grep '172.99.36.8 node2.lab.example.com node1' /etc/hosts) ]] || echo '172.99.36.8 node2.lab.example.com node2' >> /etc/hosts
 
