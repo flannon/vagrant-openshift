ad2d7819a2e32d7b7db4638c7e9cf1a9d24ef2b5
flannon <flannon@5eight5.com>
2019-01-12 22:44:35 -0500

    Added infra-node1

--- bin/hosts.sh
+++ bin/hosts.sh
@@ -3,7 +3,9 @@
 # OpenShift cluster nodes
 [[ $(grep '172.99.36.254 control.lab.example.com control' /etc/hosts) ]] || echo '172.99.36.254 control.lab.example.com control' >> /etc/hosts
 
-[[ $(grep '172.99.36.6 master.lab.example.com master' /etc/hosts) ]] || echo '172.99.36.6 master.lab.example.com master' >> /etc/hosts
+[[ $(grep '172.99.36.5 master.lab.example.com master' /etc/hosts) ]] || echo '172.99.36.5 master.lab.example.com master' >> /etc/hosts
+
+[[ $(grep '172.99.36.6 infra-node1.lab.example.com infra-node1' /etc/hosts) ]] || echo '172.99.36.6 infra-node1.lab.example.com infra-node1' >> /etc/hosts
 
 [[ $( grep '172.99.36.7 node1.lab.example.com node1' /etc/hosts) ]] || echo '172.99.36.7 node1.lab.example.com node1' >> /etc/hosts
 
